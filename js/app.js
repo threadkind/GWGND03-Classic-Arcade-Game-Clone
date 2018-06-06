634de8bca6287565359690983ff2853797e1dc0f
threadkind <threadkind@gmail.com>
2018-06-06 12:44:12 -0500

    Resolve basic conflicts to allow canvas to show

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 100;
+    this.y = 100;
 };
 
 // Update the enemy's position, required method for game
@@ -24,12 +26,27 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-horn-girl.png';
+    this.x = 20;
+    this.y = 20;
+}
+Player.prototype.update = function() {
 
+};
+Player.prototype.render = function() {
+    //ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+Player.prototype.handleInput = function() {
+
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var player = new Player();
+var allEnemies = [];
 
 
 // This listens for key presses and sends the keys to your
